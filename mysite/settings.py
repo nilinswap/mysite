ddd9959e65799924e61a9180518e891972a04c88
nilinswap <nilinswap@gmail.com>
2019-03-30 11:37:16 +0530

    like psg

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,8 @@ SECRET_KEY = '3qa*m&fo*67vacb+(kwx28lfsm#amz!ip9j)hvt-&aqco3mi1s'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
+
 
 
 # Application definition
