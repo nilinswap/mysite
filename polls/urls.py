b2d84e7bb5d556a6cf6f7d205c141dd09850b4e2
nilinswap <nilinswap@gmail.com>
2019-05-02 22:16:20 +0530

    mail

--- polls/urls.py
+++ polls/urls.py
@@ -9,6 +9,4 @@ urlpatterns = [
     url(r'^(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
     url(r'^(?P<question_id>[0-9]+)/results/$', views.results, name='results'),
     url(r'^(?P<question_id>[0-9]+)/votes/$', views.vote, name='votes')
-    
-
 ]
