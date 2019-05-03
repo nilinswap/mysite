797122361e7df7879d66de92f98e38bf045b3c08
nilinswap <nilinswap@gmail.com>
2019-05-03 12:00:08 +0530

    add shortcut

--- polls/urls.py
+++ polls/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 app_name = 'polls'
 urlpatterns = [
-    url(r'^$', views.mail, name='index'),
+    url(r'^$', views.index, name='index'),
     url(r'^mail/$', views.mail, name='mail'),
     url(r'^(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
     url(r'^(?P<question_id>[0-9]+)/results/$', views.results, name='results'),
